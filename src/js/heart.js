60c7d32b6abbfd14bcd2798517bb246535b55764
andrsweb <proflinervk@gmail.com>
2023-07-17 18:36:34 +0700

    create works desktop

--- src/js/heart.js
+++ src/js/heart.js
@@ -6,14 +6,14 @@ document.addEventListener( 'DOMContentLoaded', () => {
 
 const fillHearts = () => {
 	const cards = document.querySelectorAll( '.card' )
-	const heart = document.querySelector( '.heart' )
+	const heart = document.querySelector( '.card__heart' )
 
 	if( ! cards.length && ! heart ) return
 
 	cards.forEach( card => {
 		card.addEventListener('click', e => {
 			const target = e.target
-			const heart = target.closest('.heart')
+			const heart = target.closest('.card__heart')
 		
 			if( !heart ) return
 		
--- src/js/sliders.js
+++ src/js/sliders.js
@@ -1,22 +1,50 @@
 import Swiper from 'swiper';
-import { Pagination } from 'swiper/modules';
+import { Pagination, Navigation } from 'swiper/modules';
 
 document.addEventListener( 'DOMContentLoaded', () => {
 	'use strict'
 
-	swiperWithPagination()
+	swiperWithPagination( '.card-swiper' )
+	swiperWithUI( '.works-swiper' )
 } )
 
 const swiperWithPagination = () => {
+	let swipers = document.querySelectorAll( '.card-swiper' )
 
-	const swiper = new Swiper('.works-swiper', {
+	swipers.forEach( slider => {
+		const swiper = new Swiper( slider, {
+			grabCursor: true,
+			loop: true,
+			modules: [ Pagination ],
+	
+			pagination: {
+				el: '.card-pagination',
+				clickable: true,
+			},
+		} )
+	} )
+}
+
+const swiperWithUI = ( selector ) => {
+
+	const swiper = new Swiper( selector, {
 		grabCursor: true,
 		loop: true,
-		modules: [ Pagination ],
+		modules: [ Pagination, Navigation ],
 
 		pagination: {
-			el: '.card-pagination',
+			el: '.works-pagination',
 			clickable: true,
+			renderBullet: bullet
 		},
+
+		navigation: {
+			nextEl: '.works-next',
+			prevEl: '.works-prev'
+		}
 	} )
+}
+
+const bullet = (index, className) => {
+	return '<span class="' + className +  '">' + (index + 1) + "</span>";
 }
